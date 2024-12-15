1825597ff5d58cabfde958488563838f7b9c5952
NAHID12105080 <nahid151341@gmail.com>
2024-12-15 18:51:09 +0600

    "project basic setup done"

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { games } from "./store";
+import { startLogger } from "./logger";
+
+startLogger();
+
+setInterval(() => {
+  games.push({
+    id: Math.random().toString(),
+    whitePlayerName: "Alice",
+    blackPlayerName: "Bob",
+    moves: [],
+  });
+}, 1000);
